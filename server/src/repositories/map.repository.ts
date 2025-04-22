77afda5452b33a357ad946ad59df2600607f9e92
dgh1818 <dgh1218@outlook.com>
2025-04-22 11:44:58 +0800

    Bug Fix

--- server/src/repositories/map.repository.ts
+++ server/src/repositories/map.repository.ts
@@ -206,10 +206,11 @@ export class MapRepository implements IMapRepository {
       city = data.regeocode.addressComponent.province;
     }
     const district = data.regeocode.addressComponent.district;
-    this.logger.error(`district: ${district}`);
-    const address = data.regeocode.formatted_address;
 
-    return { country: state, state: city, city: district };
+    const address = data.regeocode.formatted_address;
+    this.logger.error(`district: ${address}`);
+
+    return { country: country, state: state, city: city };
   }
 
   async reverseGeocode(point: GeoPoint): Promise<ReverseGeocodeResult | null> {
