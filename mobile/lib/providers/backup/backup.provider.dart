c7d78612ce98c68a6f5e283988e280ab65c2dbad
dgh18 <dgh1218@outlook.com>
2025-08-13 19:17:08 +0800

    后台备份是否启用判断恢复

--- mobile/lib/providers/backup/backup.provider.dart
+++ mobile/lib/providers/backup/backup.provider.dart
@@ -377,8 +377,7 @@ class BackupNotifier extends StateNotifier<BackUpState> {
   /// which albums are selected or excluded
   /// and then update the UI according to those information
   Future<void> getBackupInfo() async {
-    //final isEnabled = await _backgroundService.isBackgroundBackupEnabled();
-    final isEnabled = false;
+    final isEnabled = await _backgroundService.isBackgroundBackupEnabled();
 
     state = state.copyWith(backgroundBackup: isEnabled);
     if (isEnabled != Store.get(StoreKey.backgroundBackup, !isEnabled)) {
