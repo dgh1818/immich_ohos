92e2b021312dddc2163432c37cc0a05ec413aa49
dgh18 <dgh1218@outlook.com>
2025-08-18 02:07:02 +0800

    默认加载原图Bug Fix

--- mobile/lib/domain/models/setting.model.dart
+++ mobile/lib/domain/models/setting.model.dart
@@ -4,8 +4,8 @@ enum Setting<T> {
   tilesPerRow<int>(StoreKey.tilesPerRow, 4),
   groupAssetsBy<int>(StoreKey.groupAssetsBy, 0),
   showStorageIndicator<bool>(StoreKey.storageIndicator, true),
-  loadOriginal<bool>(StoreKey.loadOriginal, false),
-  loadOriginalVideo<bool>(StoreKey.loadOriginalVideo, false),
+  loadOriginal<bool>(StoreKey.loadOriginal, true),
+  loadOriginalVideo<bool>(StoreKey.loadOriginalVideo, true),
   preferRemoteImage<bool>(StoreKey.preferRemoteImage, false),
   advancedTroubleshooting<bool>(StoreKey.advancedTroubleshooting, false),
   enableBackup<bool>(StoreKey.enableBackup, false);
