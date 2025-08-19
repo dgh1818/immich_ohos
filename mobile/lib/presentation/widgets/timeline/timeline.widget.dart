0726e5930441ee4a81c520a27f43ead65a83d16d
dgh18 <dgh1218@outlook.com>
2025-08-20 00:47:51 +0800

    修改预加载长度

--- mobile/lib/presentation/widgets/timeline/timeline.widget.dart
+++ mobile/lib/presentation/widgets/timeline/timeline.widget.dart
@@ -278,7 +278,7 @@ class _SliverTimelineState extends ConsumerState<_SliverTimeline> {
           final grid = CustomScrollView(
             primary: true,
             physics: _scrollPhysics,
-            cacheExtent: maxHeight * 2,
+            cacheExtent: maxHeight * 6,
             slivers: [
               if (isSelectionMode) const SelectionSliverAppBar() else if (widget.appBar != null) widget.appBar!,
               if (widget.topSliverWidget != null) widget.topSliverWidget!,
