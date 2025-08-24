e82cf7a15c05e9684b7e9c0bc6319f0047c94e37
dgh18 <dgh1218@outlook.com>
2025-08-25 07:57:28 +0800

    Fix部分设备无法登陆的问题2

--- mobile/lib/services/api.service.dart
+++ mobile/lib/services/api.service.dart
@@ -176,8 +176,8 @@ class ApiService implements Authentication {
       authenticationApi.apiClient.addDefaultHeader('deviceModel', iosInfo.utsname.machine);
       authenticationApi.apiClient.addDefaultHeader('deviceType', 'iOS');
     } else if (defaultTargetPlatform == TargetPlatform.ohos) {
-      final ohosInfo = await deviceInfoPlugin.ohosInfo;
-      authenticationApi.apiClient.addDefaultHeader('deviceModel', ohosInfo.marketName ?? "Unkonwn HMOS device");
+      //final ohosInfo = await deviceInfoPlugin.ohosInfo;
+      authenticationApi.apiClient.addDefaultHeader('deviceModel', "HMOS device");
       authenticationApi.apiClient.addDefaultHeader('deviceType', 'OHOS');
     } else if (Platform.isAndroid) {
       final androidInfo = await deviceInfoPlugin.androidInfo;
