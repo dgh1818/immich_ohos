c3cfbcbacf955305de238b30b940353c7b43c101
dgh18 <dgh1218@outlook.com>
2025-09-09 10:59:59 +0800

    [Feature]:Exif地理位置修改恢复 patch2

--- mobile/lib/pages/search/map/map_location_picker.page.dart
+++ mobile/lib/pages/search/map/map_location_picker.page.dart
@@ -66,6 +66,11 @@ class MapLocationPickerPage extends HookConsumerWidget {
       var currentLatLng = LatLng(currentLocation.latitude, currentLocation.longitude);
       selectedLatLng.value = currentLatLng;
       controller.value?.animateCamera(CameraUpdate.newLatLng(currentLatLng));
+
+      if (defaultTargetPlatform == TargetPlatform.ohos) {
+        ByteData mapMarkData = await rootBundle.load("assets/location-pin.png");
+        await controller.value?.addMarkerAtLatLng_Ohos(currentLatLng, mapMarkData, 0.15);
+      }
     }
 
     return MapThemeOverride(
